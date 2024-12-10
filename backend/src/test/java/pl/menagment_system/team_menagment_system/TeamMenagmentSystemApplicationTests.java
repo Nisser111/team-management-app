5d16ba9eb4f27da1476e6c856014f96dc643b9e4
Miłosz <kulakmilosz2006@gmail.com>
2024-12-10 20:55:24 +0100

    Initial state of backend

---
+++ backend/src/test/java/pl/menagment_system/team_menagment_system/TeamMenagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.menagment_system.team_menagment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMenagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
