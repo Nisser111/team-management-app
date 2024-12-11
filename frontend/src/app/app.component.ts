7fb4a1dd07ef7eb32d80a26ee54d8283418afc8c
Miłosz <kulakmilosz2006@gmail.com>
2024-12-11 20:41:02 +0100

    Move hello user header to his component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,19 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { FilterHeaderComponent } from "../components/filter-header/filter-header.component";
+import { HelloUserHeaderComponent } from "../components/hello-user-header/hello-user-header.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, FilterHeaderComponent],
+  imports: [RouterOutlet, FilterHeaderComponent, HelloUserHeaderComponent],
   template: `
-    <header id="app-header">
-      <h1>
-        Witaj <span id="username">{{ username }}</span
-        >! Zarządzaj swoimi zespołami.
-      </h1>
-    </header>
+    <app-hello-user-header></app-hello-user-header>
 
-    <app-filter-header></app-filter-header>
+    <main>
+      <app-filter-header></app-filter-header>
+    </main>
 
     <router-outlet />
   `,
-  styles: [],
 })
-export class AppComponent {
-  username = "Miłosz"; // Temp
-}
+export class AppComponent {}
