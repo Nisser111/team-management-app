d37b6f2d0ce695a1861feea2f5a5fecec02fa21b
Miłosz <kulakmilosz2006@gmail.com>
2024-12-13 19:51:21 +0100

    Add post, patch and delete method for team service

--- frontend/src/services/teams.service.ts
+++ frontend/src/services/teams.service.ts
@@ -11,11 +11,53 @@ export class TeamService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Retrieves a list of all teams from the server.
+   *
+   * @returns An Observable that resolves to an array of Team objects.
+   */
   getTeams(): Observable<Team[]> {
     const headers = {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
     };
     return this.http.get<Team[]>(`${this.apiUrl}/teams`, { headers });
+  }
+
+  /**
+   * Posts a new team to the server.
+   *
+   * @param team - The Team object to be added.
+   * @returns An Observable that resolves to the added Team object.
+   */
+  addTeam(team: Team): Observable<Team> {
+    return this.http.post<Team>(`${this.apiUrl}/teams`, team, {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  /**
+   * Updates an existing team by its ID.
+   *
+   * @param id - The ID of the team to be updated.
+   * @param team - The updated Team object.
+   * @returns An Observable that resolves to the updated Team object.
+   */
+  updateTeam(id: string, team: Team): Observable<Team> {
+    return this.http.patch<Team>(`${this.apiUrl}/teams/${id}`, team, {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  /**
+   * Deletes a team by its ID.
+   *
+   * @param id - The ID of the team to be deleted.
+   * @returns An Observable that resolves to void.
+   */
+  deleteTeam(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/teams/${id}`, {
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
