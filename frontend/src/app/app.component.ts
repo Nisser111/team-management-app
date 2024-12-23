36926c4173a5995601eaf299cb4c33392442d18d
Miłosz <kulakmilosz2006@gmail.com>
2024-12-23 11:52:54 +0100

    Refactor main app function from app.component to team-menagement component.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,34 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { FilterHeaderComponent } from "../components/filter-header/filter-header.component";
-import { HelloUserHeaderComponent } from "../components/hello-user-header/hello-user-header.component";
-import { TeamsContainerComponent } from "../components/teams-container/teams-container.component";
+import { TeamMenagementComponent } from "../components/team-menagement/team-menagement.component";
 
 @Component({
   selector: "app-root",
-  imports: [
-    RouterOutlet,
-    FilterHeaderComponent,
-    HelloUserHeaderComponent,
-    TeamsContainerComponent,
-  ],
+  imports: [RouterOutlet, TeamMenagementComponent],
   template: `
-    <app-hello-user-header></app-hello-user-header>
-    <app-filter-header
-      (teamSelected)="onTeamSelected($event)"
-    ></app-filter-header>
-    <main>
-      <app-teams-container
-        [selectedTeamId]="selectedTeamId"
-      ></app-teams-container>
-    </main>
+    <app-team-menagement></app-team-menagement>
     <router-outlet />
   `,
 })
-export class AppComponent {
-  selectedTeamId: number = -1;
-
-  onTeamSelected(teamId: number) {
-    this.selectedTeamId = teamId;
-  }
-}
+export class AppComponent {}
