41b0819713a99ccc35abb91b240931d1f11107c8
Miłosz <kulakmilosz2006@gmail.com>
2025-01-27 13:14:21 +0100

    Add logger using logback and actuator. Make JobNumberFilter to add job number. Set /logs/ directory to gitignore.

---
+++ backend/src/main/java/pl/menagment_system/team_menagment_system/config/JobNumberFilter.java
@@ -0,0 +1,39 @@
+package pl.menagment_system.team_menagment_system.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class JobNumberFilter implements Filter {
+
+    private static final String JOB_NUMBER_KEY = "Job-number";
+
+    @Override
+    public void doFilter(jakarta.servlet.ServletRequest request,
+            jakarta.servlet.ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        String jobNumber = UUID.randomUUID().toString();
+
+        MDC.put(JOB_NUMBER_KEY, jobNumber);
+
+        if (response instanceof HttpServletResponse) {
+            ((HttpServletResponse) response).setHeader(JOB_NUMBER_KEY, jobNumber);
+        }
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove(JOB_NUMBER_KEY);
+        }
+    }
+}
