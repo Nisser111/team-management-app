2a952cea829d85de070015d447734433523d9782
Miłosz <kulakmilosz2006@gmail.com>
2025-01-27 21:25:36 +0100

    Add microservice to generate summary fo employees and configure db

---
+++ summaryGenerator.microsevice/src/test/java/pl/summaryGenerator/SummaryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.summaryGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
