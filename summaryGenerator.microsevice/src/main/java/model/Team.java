cb4d89fc8473061bb225012b5540141d6f710e9d
Miłosz <kulakmilosz2006@gmail.com>
2025-01-27 21:30:46 +0100

    Copy models and rebuild repositories from main api service

---
+++ summaryGenerator.microsevice/src/main/java/model/Team.java
@@ -0,0 +1,33 @@
+package model;
+
+
+/**
+ * The Team class represents a team entity with attributes for unique identification
+ * and a name.
+ */
+public class Team {
+
+    private int id;
+    private String name;
+
+    public Team(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Object value) {
+        this.id = (int) value;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(Object value) {
+        this.name = (String) value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
